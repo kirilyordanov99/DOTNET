814a7b2c33167aa8af26f485ddd484c4e80047fb
Kiril Yordanov <32341596+kirilyordanov99@users.noreply.github.com>
2023-09-13 19:58:51 +0300

    Delete Web Application/Models/EmailSenderOptions.cs

--- Web Application/Models/EmailSenderOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Web_Application.Models
-{    //password reset
-    public class EmailSenderOptions
-    {
-        public required string FromEmail { get; set; }
-        public required string FromName { get; set; }
-        public required string SendGridApiKey { get; set; }
-    }
-}
